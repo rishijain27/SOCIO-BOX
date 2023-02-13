2864bd8654963a303c0d028988ea9c945c940f21
rishijain27 <110525174+rishijain27@users.noreply.github.com>
2023-02-13 20:00:48 +0530

    capital PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require("./config/keys");
 
 mongoose.set("strictQuery", true);
